09b666aae8e3d00f5710d2772939eee933f4a418
Dennis <dennisgdaniels@gmail.com>
2022-10-09 15:24:30 +0400

    working fastapi with HTML jinja templating

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fastapi_simple_jinja
+inspired by https://www.youtube.com/watch?v=IxXtDOI9RUo
+
---
+++ app.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+# https://pypi.org/project/Jinja2/
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get('/')
+def hello_world():
+    return { 'message': 'hello' }
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
+if __name__ == '__main__':
+    uvicorn.run(app)
